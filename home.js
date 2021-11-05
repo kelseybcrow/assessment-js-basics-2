a441b088c07812b76c942199c0ef94774b2373c9
Kelsey Crow <kelseybcrow@gmail.com>
2021-11-05 09:31:54 -0600

    finished home

--- home.js
+++ home.js
@@ -7,7 +7,6 @@
     page of the restaurant's website.
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /* 
     Write an *arrow* function called `greetUser`
@@ -23,9 +22,9 @@
 
 //CODE HERE
 
+const greetUser = (username) => `Welcome back, ${username}`;
 
-
-
+console.log(greetUser("Betsy"));
 
 //////////////////PROBLEM 2////////////////////
 /* 
@@ -47,11 +46,17 @@
         // `You're in our delivery zone!`
 */
 
-const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
+const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206];
 
 //CODE HERE
 
-
+const canWeDeliver = (zipCode) => {
+  if (deliveryAreaZipCodes.includes(zipCode)) {
+    return "You are eligible for delivery";
+  } else {
+    return "Unfortunately, you are not eligible for delivery";
+  }
+};
 
 /* 
     Problem 2 Continued
@@ -72,6 +77,19 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 
 // CODE HERE
 
+const canWeDeliver2 = (zipCode) => {
+  let deliver = false;
+  for (let i = 0; i < deliveryAreaZipCodes.length; i++) {
+    if (deliveryAreaZipCodes[i] === zipCode) {
+      deliver = true;
+    }
+  }
+  if (deliver === true) {
+    return "You are eligible for delivery";
+  } else {
+    return "Unfortunately, you are not eligible for delivery";
+  }
+};
 
 //////////////////PROBLEM 3////////////////////
 /* 
@@ -87,15 +105,15 @@ const deliveryAreaZipCodes = [85205, 85204, 85203, 85213, 85206]
 */
 
 const deals = [
-    {
-        title: '15% Off!', 
-        desc: 'Applied to your entire order when you spend $30 or more'
-    }, 
-    {
-        title: 'Free Kids Meal with 2 Regular Entrees', 
-        desc: '   This deal lasts until the end of March! '
-    }
-]
+  {
+    title: "15% Off!",
+    desc: "Applied to your entire order when you spend $30 or more",
+  },
+  {
+    title: "Free Kids Meal with 2 Regular Entrees",
+    desc: "   This deal lasts until the end of March! ",
+  },
+];
 
 /*
     The owner has decided to take the 15% off
@@ -108,7 +126,7 @@ const deals = [
 
 //CODE HERE
 
-
+deals[0].title.replace("15", "10");
 
 /*
     The restaurant is going to continue its
@@ -124,3 +142,4 @@ const deals = [
 */
 
 //CODE HERE
+deals[1].desc.replace("March", "April").trim();
